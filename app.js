17c27e685edc314ceac8bc6a832c3f3eca3d27e1
ScrabyQ <iameeeboy@gmail.com>
2021-11-07 04:58:06 +0300

    adding routing and start server

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const Passwd = require("./modules/passgen")
+const Passwd = require("./modules/passgen");
+const express = require('express');
+const app = express();
 
-const pass = new Passwd(3);
+const port = process.env.PORT || 5000;
 
-for (let i = 0; i <= 9; i++){
-    console.log(pass.generate({ LC_EN: true}))
-}
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`)
+})
+
+app.post('/get_password' ,(req, res) => {
+    const pass = new Passwd();
+    pass.length = +req.body.length
+    let arr = []
+    for (let i = 0; i < 10; i++){
+
+        arr.push(pass.generate(req.body))
+    }
+
+    res.status(200).json(JSON.stringify(arr))
+
+})
+
 
